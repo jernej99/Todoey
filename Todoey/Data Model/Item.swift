0e98b6ea696622b624120d35d45570733d989a62
jernej.mihalic <jernej_23@hotmail.com>
2018-11-22 10:41:52 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by jernej mihalic on 22/11/2018.
+//  Copyright © 2018 jernej mihalic. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
